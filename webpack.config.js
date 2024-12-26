e4cb3d3df3baad466755f7af84b25fbd51b5a79b
MOHAMED SESAY <162488876+deeeye2@users.noreply.github.com>
2024-12-26 16:16:05 +0000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -15,4 +15,12 @@ module.exports = {
       },
     ],
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
 };
+
