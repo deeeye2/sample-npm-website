b0876dc9cdf32e01cf20087023970a6ec395ce01
MOHAMED SESAY <162488876+deeeye2@users.noreply.github.com>
2024-12-26 15:47:04 +0000

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const appDiv = document.createElement('div');
+  appDiv.textContent = 'Hello, this is a sample NPM website!';
+  appDiv.className = 'app';
+  document.body.appendChild(appDiv);
+});
